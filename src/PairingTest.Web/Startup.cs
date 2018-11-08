9863f494460caeb3ec2c8ef924d88a44ce81e8b8
umairdojki <umair.dojki@stepstone.com>
2018-11-08 17:56:15 +0000

    Add URL for QuestionServiceWebApi in appsettings, and inject the dependency for Configuration

--- src/PairingTest.Web/Startup.cs
+++ src/PairingTest.Web/Startup.cs
@@ -18,6 +18,7 @@ namespace PairingTest.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
